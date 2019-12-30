051ffc4496511bcf21d08b4e49e50b1a9cc64533
Andre Morrall <andre.morrall@corgan.com>
2019-12-30 16:01:41 -0600

    Recreate database to reset product quantity count

--- bamazon.sql
+++ bamazon.sql
@@ -14,31 +14,31 @@ CREATE TABLE products (
 );
 -- electronics --
 INSERT INTO products (product_name, dept_name, price, stock_qty)
-VALUES ('airpods', 'electronics', 150, 100);
+VALUES ('Airpods', 'Electronics', 150, 100);
 INSERT INTO products (product_name, dept_name, price, stock_qty)
-VALUES ('pixel 4xl', 'electronics', 1000, 4);
+VALUES ('Pixel 4xl', 'Electronics', 1000, 4);
 
 -- shoes --
 INSERT INTO products (product_name, dept_name, price, stock_qty)
-VALUES ('air max', 'shoes', 130, 10);
+VALUES ('Air Max', 'Shoes', 130, 10);
 INSERT INTO products (product_name, dept_name, price, stock_qty)
-VALUES ('nike lebron', 'shoes', 150, 1);
+VALUES ('Nike Lebron', 'Shoes', 150, 1);
 
 -- appliances --
 INSERT INTO products (product_name, dept_name, price, stock_qty)
-VALUES ('keurig k-mini', 'appliances', 95, 45);
+VALUES ('Keurig K-mini', 'Appliances', 95, 45);
 INSERT INTO products (product_name, dept_name, price, stock_qty)
-VALUES ('ninja bl480 blender', 'appliances', 50, 85);
+VALUES ('Ninja BL480 Blender', 'Appliances', 50, 85);
 
 -- clothing --
 INSERT INTO products (product_name, dept_name, price, stock_qty)
-VALUES ('wool sweater', 'clothing', 20, 200);
+VALUES ('Wool sweater', 'Clothing', 20, 200);
 INSERT INTO products (product_name, dept_name, price, stock_qty)
-VALUES ('fleece sweatshirt', 'clothing', 15, 150);
+VALUES ('Fleece sweatshirt', 'Clothing', 15, 150);
 
 -- books -- 
 INSERT INTO products (product_name, dept_name, price, stock_qty)
-VALUES ('eloquent javascript', 'books', 27, 1);
+VALUES ('Eloquent Javascript', 'Books', 27, 1);
 INSERT INTO products (product_name, dept_name, price, stock_qty)
-VALUES ('the millionaire next door', 'books', 11, 10);
+VALUES ('Millionaire Next Door', 'Books', 11, 10);
 
